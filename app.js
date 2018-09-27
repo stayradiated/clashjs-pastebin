8b1c61ace2c8afa0aac3174279da55a8d8fd1e78
George Czabania <george@mish.guru>
2018-09-27 17:53:41 +1200

    Customise for Node.js bots

--- app.js
+++ app.js
@@ -1,84 +1,90 @@
-var express = require('express');
-var fs = require('fs');
+var express = require('express')
+var fs = require('fs')
 var bodyParser = require('body-parser')
-var app = express(); 
+var app = express()
+var sanitize = require('sanitize-filename')
+var join = require('path').join
 
-
-var port = 5000;
+var port = 5000
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+app.use(express.static('public'))
 
-app.use(express.static('public'));
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
 
-app.set('views','./src/views');
-app.set('view engine','ejs');	
+app.get('/', function(req, res) {
+  res.render('index', { title: 'glupaste' })
+})
 
-app.get('/', function(req,res){
-	res.render('index', { title: 'glupaste' });
-	// res.sendFile(__dirname + '/src/views/test.html');
-});
+app.get('/:id', function(req, res) {
+  var path = __dirname + '/files/' + req.params.id + '.txt'
+  fs.readFile(path, 'utf8', function(err, data) {
+    if (err) {
+      res.render('nopaste')
+    } else {
+      var metadata = JSON.parse(data)
+      var author = metadata.author
+      var language = metadata['language']
+      var content = metadata['content']
+      var timestamp = metadata.timestamp
+      var date = metadata.date
 
-app.get('/:id', function(req,res){
-	var path = __dirname + "/files/" + req.params.id + '.txt';
-	fs.readFile(path, 'utf8', function (err,data) {
-  		if (err) {
-    		res.render('nopaste');
-  		}else{
+      res.render('paste', {
+        author: author,
+        language: language,
+        content: content,
+        timestamp: timestamp,
+        date: date
+      })
+    }
+  })
+})
 
+app.post('/', urlencodedParser, function(req, res) {
+  var author = req.body.poster
+  var language = req.body.syntax
+  var content = req.body.content
+  var timestamp = Date.now()
+    .toString()
+    .substring(6)
+  var date = Date()
 
-	  		var metadata = JSON.parse(data);
-	  		var author = metadata.author;
-	  		var language = metadata['language'];
-	  		var content = metadata['content'];
-	  		var timestamp =  metadata.timestamp;
-	  		var date = metadata.date;
+  var metadata = {
+    author: author,
+    language: language,
+    content: content,
+    timestamp: timestamp,
+    date: date
+  }
 
-	  		res.render('paste',{
-	  			author: author,
-	  			language: language,
-	  			content: content,
-	  			timestamp : timestamp,
-	  			date : date
-			});
-		}		
-	});
-});
+  const filename = `${timestamp}.txt`
+  const path = `${__dirname}/files/${filename}`
 
+  const jsFilename = `${sanitize(author)}_${timestamp}.js`
+  const jsPath = join(process.env.PLAYER_DIR, jsFilename)
 
-app.post('/', urlencodedParser , function(req,res){
-	
-	var author = req.body.poster;	
-	var language = req.body.syntax;	
-	var content = req.body.content;
-	var timestamp = Date.now().toString().substring(6);
-	var date = Date();
-	
+  fs.writeFile(jsPath, content, function (err) {
+    if (err) {
+      console.log(err)
+    }
+    console.log(`File saved: ${jsPath}`)
+  })
 
-	var metadata = {
+  fs.writeFile(path, JSON.stringify(metadata, null, 4), function(err) {
+    if (err) {
+      return console.log(err)
+    }
+    res.redirect('/' + timestamp)
+    console.log(`File saved: ${filename}`)
+  })
+})
 
-		author : author,
-		language : language,
-		content : content,
-		timestamp : timestamp,
-		date : date
-	};
-	var path = __dirname + "/files/" + timestamp+ '.txt';
+app.get('*', function(req, res) {
+  res.render('error')
+})
 
-	fs.writeFile(path, JSON.stringify(metadata,null,4), function (err) {
-  		if (err) return console.log(err);
-  			res.redirect('/' + timestamp);
-  	});
-	console.log('File saved-' + timestamp+'.txt');
-
-});
-
-app.get('*', function(req, res){
-  res.render('error');
-});
-
-
-app.listen(port , function(err)	{
-	console.log('running server on port:'+ port);
-
-}); 	
+app.listen(port, function(err) {
+  console.log('running server on port:' + port)
+})
